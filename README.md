e8004f0b009c9f49f837e2a747f6fe5d288fddee
DESKTOP-LRKIFMR\UI1 <209315540@qq.com>
2023-03-22 18:02:11 +0800

    feat: 初始化项目

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # design-ui
+
+## TypeScript
+
+### 高级类型
+
+#### Partial
+
+把某个接口类型中定义的属性变成可选的 Optional
+
+```ts
+type Partial<T> = {
+  [P in keyof T]?: T[P];
+}
+```
+
+#### Omit
+
+https://www.jianshu.com/p/fc10a22fec6e
+在Type中选取所有的属性值，然后移除属性名在Keys中的属性值
+
+```ts
+// keyof any is string | number | symbol
+type Omit<T, K extends keyof any> = {
+  [P in Exclude<keyof T, K>]: T[P];
+}
+```
--- play/src/App.tsx
+++ play/src/App.tsx
@@ -3,11 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Button from '@design-ui/components/button'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Button />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
